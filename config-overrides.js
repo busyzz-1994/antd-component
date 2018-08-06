9b63466dfa6dc76569c18b75e7d8d060b682f710
Vincent <const.lomo@gmail.com>
2018-08-06 17:24:14 +0800

    remove: ts

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,8 @@
  * Created by Vincent on 2018/8/6.
  */
 const { compose } = require('react-app-rewired');
-const rewireLess = require("react-app-rewire-less-modules");
 const rewireMobX = require('react-app-rewire-mobx');
 
 module.exports = compose(
-  rewireLess,
   rewireMobx
-...
 );
