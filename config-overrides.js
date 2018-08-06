3e452918dc44cf5a1b9403bc980b9f6685eb67c8
Vincent <const.lomo@gmail.com>
2018-08-06 17:48:13 +0800

    fix: typO

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@
  * Created by Vincent on 2018/8/6.
  */
 const { compose } = require('react-app-rewired');
-const rewireMobX = require('react-app-rewire-mobx');
+const rewireMobx = require('react-app-rewire-mobx');
 
 module.exports = compose(
   rewireMobx
