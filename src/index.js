224488674234e71060a3c63fdaca8665f90bb63c
Vincent <const.lomo@gmail.com>
2018-08-23 14:54:04 +0800

    add: consolev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import consolev from 'consolev';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+consolev(process.env.REACT_APP_NAME, process.env.REACT_APP_PROXY);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
