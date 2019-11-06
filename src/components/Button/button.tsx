ff8632f6d6877a412dad3669340dd7a6f0b6ace7
qingzhizhi <qingzhizhi@bdfint.com>
2019-11-06 18:11:21 +0800

    'feat:button'

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,22 +4,85 @@ import { tuple } from '../utils';
 import classNames from 'classnames';
 import { getPrefix } from '../utils';
 import './style/index.scss';
-const ButtonTypes = tuple('default', 'primary', 'danger');
+const ButtonTypes = tuple('default', 'primary', 'danger', 'success');
 type ButtonType = (typeof ButtonTypes)[number];
+const ButtonShapes = tuple('round');
+type ButtonShape = (typeof ButtonShapes)[number];
+const ButtonSizes = tuple('large', 'default', 'small');
+type ButtonSize = (typeof ButtonSizes)[number];
 interface BaseButtonProps {
   type?: ButtonType;
+  shape?: ButtonShape;
+  size?: ButtonSize;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick?: React.MouseEventHandler;
+  className?: React.CSSProperties;
+  delay?: number;
+  block?: boolean;
 }
-export default class extends Component<BaseButtonProps> {
+interface ButtonState {
+  clicked: boolean;
+}
+export default class extends Component<BaseButtonProps, ButtonState> {
   static defaultProps = {
-    type: 'default'
+    type: 'default',
+    size: 'default'
+  };
+  state = {
+    clicked: false
+  };
+  handleClick = e => {
+    let { loading, onClick } = this.props;
+    if (loading) return;
+    onClick && onClick(e);
+    this.setClicked();
+  };
+  setClicked = () => {
+    this.setState(
+      {
+        clicked: true
+      },
+      () => {
+        let timer = setTimeout(() => {
+          this.setState(
+            {
+              clicked: false
+            },
+            () => {
+              clearInterval(timer);
+            }
+          );
+        }, 300);
+      }
+    );
   };
   render() {
-    let { type } = this.props;
+    let {
+      type,
+      children,
+      shape,
+      size,
+      className,
+      loading: pLoading,
+      loading,
+      block,
+      ...rest
+    } = this.props;
+    let { clicked } = this.state;
     const prefixCls = getPrefix('btn');
-    console.log(prefixCls);
-    const classes = classNames(prefixCls, {
-      [`${prefixCls}-${type}`]: type
+    const classes = classNames(prefixCls, className, {
+      [`${prefixCls}-${type}`]: type,
+      [`${prefixCls}-clicked`]: clicked,
+      [`${prefixCls}-size-${size}`]: size,
+      [`${prefixCls}-size-${size}-${shape}`]: shape,
+      [`${prefixCls}-loading`]: loading,
+      [`${prefixCls}-block`]: block
     });
-    return <button className={classes}>{type}</button>;
+    return (
+      <button {...rest} className={classes} onClick={this.handleClick}>
+        {children}
+      </button>
+    );
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import Mbutton from 'components/Button';
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
-export default class extends Component {
+interface IState {
+  loading: boolean;
+}
+export default class extends Component<any, IState> {
+  state = {
+    loading: false
+  };
   render() {
     return (
       <div style={{ margin: 10 }}>
-        <Mbutton type="danger"></Mbutton>
+        <Mbutton type="primary" loading={this.state.loading} delay={1000}>
+          thisssss
+        </Mbutton>
         <div style={{ marginTop: 10 }}>
-          <Button type="primary">yuans</Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              this.setState(prevState => ({
+                loading: !prevState.loading
+              }));
+            }}
+          >
+            yuans
+          </Button>
         </div>
       </div>
     );
