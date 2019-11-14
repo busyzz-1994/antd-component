ad8387b605039b954be37892e932d980d8aca041
qingzhizhi <qingzhizhi@bdfint.com>
2019-11-14 18:18:01 +0800

    'feat:input'

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import { getPrefix } from '../_util';
-export default class extends Component {
-  render() {
+import { getPrefix, Omit, tuple } from '../_util';
+import omit from 'omit.js';
+export const inputSize = tuple('small', 'default', 'large');
+export type InputSize = (typeof inputSize)[number];
+interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  size?: InputSize;
+  addonAfter?: React.ReactNode | string;
+  addonBefore?: React.ReactNode | string;
+}
+export default class extends Component<InputProps> {
+  renderInput() {
+    const { className, size, ...rest } = this.props;
     const prefixCls = getPrefix('input');
-    const classes = classNames(prefixCls, {
-      [`${prefixCls}-input`]: true
-    });
-    return <input className={classes} />;
+    const classes = classNames(
+      prefixCls,
+      {
+        [`${prefixCls}`]: true,
+        [`${prefixCls}-${size}`]: size
+      },
+      className
+    );
+    const otherProps = omit(rest, []);
+    return <input {...otherProps} className={classes} />;
+  }
+  render() {
+    return this.renderInput();
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import './index.scss';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
+import { Input, Icon } from 'antd';
 import MInput from '../components/Input';
 interface IState {
   loading: boolean;
+  value: string;
 }
 export default class extends Component<any, IState> {
   state = {
-    loading: false
+    loading: false,
+    value: ''
   };
-
+  inputHandler = e => {
+    let value = e.target.value;
+    this.setState({ value });
+  };
   render() {
     return (
       <div>
         <div style={{ overflowX: 'hidden', marginBottom: 10 }}>
           <div style={{ position: 'relative', margin: 10 }}>
-            <MInput />
+            <MInput
+              onChange={this.inputHandler}
+              size={'large'}
+              value={this.state.value}
+            />
           </div>
           <div style={{ position: 'relative', margin: 10 }}>
-            <Input />
+            <Input
+              suffix={
+                <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+              }
+            />
           </div>
         </div>
       </div>
