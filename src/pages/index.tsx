7ebc75b788fa827fe21b470b9c3f30c3fcbcb4d4
qingzhizhi <qingzhizhi@bdfint.com>
2019-12-02 18:22:35 +0800

    'fix:all'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,42 +3,46 @@ import './index.scss';
 import 'antd/dist/antd.css';
 import { Radio, Tooltip } from 'antd';
 import MRadio from '../components/Radio';
-
+import Trigger from 'rc-trigger';
+import { areRangesOverlapping } from 'date-fns';
 interface IState {
   loading: boolean;
   value: number;
   checked: boolean;
   defaultCheckedList?: Array<any>;
+  popupVisible: boolean;
 }
-// let a: any = 'string';
-// let a: unknown = function () {
-//   console.log('enter')
-// }
-// if (typeof a === 'function') {
-//   a()
-// }
-// let a: unknown = 'string';
-// if (typeof a === 'string') {
-//   a.split('-')
-// }
-// function isBoolean(value: unknown): value is number[]{
-//   return Array.isArray(value) && value.every(item => typeof item === 'number');
-// }
 
 export default class extends Component<any, IState> {
   state = {
     loading: false,
     value: 1,
-    checked: false
+    checked: false,
+    popupVisible: true
+  };
+  align = align => {
+    // console.log(align);
+  };
+  onPopupVisibleChange = value => {
+    this.setState({
+      popupVisible: value
+    });
+    console.log(value);
   };
   render() {
-    let { value } = this.state;
+    let { value, popupVisible } = this.state;
+    console.log(popupVisible);
     let ele = <div style={{ height: 100 }}>sdjiajsdi</div>;
     return (
       <div>
         <div style={{ marginBottom: 100 }}>
           <div style={{ position: 'relative', marginTop: 100 }}>
-            <Tooltip autoAdjustOverflow={true} trigger="click" title={'ss'}>
+            <Tooltip
+              visible={true}
+              autoAdjustOverflow={true}
+              trigger="click"
+              title={'ss'}
+            >
               <div>thishh</div>
             </Tooltip>
             <Tooltip autoAdjustOverflow={true} trigger="click" title={'ddd'}>
@@ -48,7 +52,25 @@ export default class extends Component<any, IState> {
           <div style={{ position: 'relative', margin: 10 }}>
             <Radio>ccc</Radio>
           </div>
-          <div className="test-box"></div>
+          <div className="test-box">
+            <Trigger
+              action={['hover']}
+              popupStyle={{ position: 'absolute' }}
+              prefixCls="busyzz-trigger-popup"
+              popup={<span style={{ background: '#f0f' }}>popup</span>}
+              getPopupClassNameFromAlign={this.align}
+              popupAlign={{
+                points: ['tl', 'bl'],
+                offset: [0, 3]
+              }}
+              onPopupVisibleChange={this.onPopupVisibleChange}
+              mask={true}
+              // popupVisible={popupVisible}
+              popupVisible={popupVisible}
+            >
+              <a href="#">hover</a>
+            </Trigger>
+          </div>
         </div>
       </div>
     );
